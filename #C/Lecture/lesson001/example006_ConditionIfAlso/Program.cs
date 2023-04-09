ffaca1411e7442a26e6c1511da0f41047a5bbe67
Kseniya_Sergeevna <kotlyaginaks@mail.ru>
2023-04-09 23:03:28 +0700

    1

---
+++ #C/Lecture/lesson001/example006_ConditionIfAlso/Program.cs
@@ -0,0 +1,13 @@
+﻿// Особое приветствие
+Console.Write("Введите имя пользователя: ");
+ string username = Console.ReadLine();
+
+ if (username.ToLower() == "паша")
+ {
+    Console.WriteLine("Ура, это же ПАША!");
+ }
+ else
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+ }
