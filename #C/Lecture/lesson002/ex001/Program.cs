f8edf1cfde94499d2c040fb312dbbc433d0b2756
Kseniya_Sergeevna <kotlyaginaks@mail.ru>
2023-04-11 20:02:47 +0700

    1

---
+++ #C/Lecture/lesson002/ex001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Находим максимум из 9 чисел 
+
+
+int a1 = 13;
+int b1 = 16;
+int c1 = 1;
+
+int a2 = 122;
+int b2 = 126;
+int c2 = 51;
+
+
+int a3 = 7;
+int b3 = 76;
+int c3 = 9;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
