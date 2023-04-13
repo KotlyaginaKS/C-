35a7cb5db653ac613eeed43ea0ffaee0b4103473
Kseniya_Sergeevna <kotlyaginaks@mail.ru>
2023-04-13 13:16:58 +0700

    1

---
+++ #C/Seminar/lesson002/ex002/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Программа, которая выводит случайное трехзначное число и показывает вторую цифру этого числа 
+
+int GetRandonDigit (int minDigit = 100, int maxDigit = 999)
+{
+    int num = new Random().Next(minDigit, maxDigit + 1);
+    return num;
+}
+
+int DeletSecondDigit(int num)
+{
+    num = num / 10;
+    num = num % 10;
+    return num;
+}
+
+int num = GetRandonDigit();
+Console.WriteLine(num);
+Console.WriteLine(DeletSecondDigit(num));
