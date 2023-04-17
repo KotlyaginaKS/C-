ac251573693cea0f992aa94f945d062d788b2888
Kseniya_Sergeevna <kotlyaginaks@mail.ru>
2023-04-17 15:20:34 +0700

    1

--- #C/HomeWork/003/ex001/Program.cs
+++ #C/HomeWork/003/ex001/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 Console.WriteLine("Введите пятизначное число: ");
-string? number = Console.ReadLine();
+string number = Console.ReadLine();
 
 void CheckingNumber(string number)
 {
