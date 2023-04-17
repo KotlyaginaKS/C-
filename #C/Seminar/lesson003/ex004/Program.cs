30c7052515b1afe89dbe80fcbf2f924e5bc5ea7d
Kseniya_Sergeevna <kotlyaginaks@mail.ru>
2023-04-17 12:08:31 +0700

    1

---
+++ #C/Seminar/lesson003/ex004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая принимает на вход координаты двух точек и находит рассстояние между ними в 2Д пространстве
+
+double DistanseXY(double ax, double bx, double ay, double by)
+{
+double distanse = Math.Pow(ax-ay, 2) + Math.Pow(bx-by, 2);
+distanse = Math.Sqrt(distanse);
+return distanse;
+}
+
+System.Console.WriteLine ("Введите первое значение координаты по оси Х ");
+double ax = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine ("Введите второе значение координаты по оси Y ");
+double bx = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine ("Введите первое значение координаты по оси X ");
+double ay = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine ("Введите второе значение координаты по оси Y ");
+double by = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Расстояние между точками равно: " + DistanseXY(ax, bx, ay, by));
